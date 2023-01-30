18c97eded618f9e0c4e5ac0b75f71939a2a952c2
fengtianhe <fengtianhe@58.com>
2023-01-30 19:27:34 +0800

    展示模版

--- src/main/java/com/fengtianhe/shareanalysis/controller/TemplateController.java
+++ src/main/java/com/fengtianhe/shareanalysis/controller/TemplateController.java
@@ -3,7 +3,6 @@ package com.fengtianhe.shareanalysis.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class TemplateController {
@@ -11,7 +10,7 @@ public class TemplateController {
      * 每日特殊指标
      * @return
      */
-    @RequestMapping(value = "/special-index-daily", method = RequestMethod.GET)
+    @RequestMapping(value = "/special-index-daily")
     public String specialIndexDaily() {
         return "SpecialIndexDaily";
     }
